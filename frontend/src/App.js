94eed29392414820f1a581645f6e958c48c6f5cb
Harsh Malviya <harshmalviya72@gmail.com>
2022-02-04 02:50:54 +0530

    Added cart functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 import Homescreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
         <Container>
           <Route path="/" component={Homescreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
