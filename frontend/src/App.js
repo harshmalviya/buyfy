449c7e7cfd19135b3bb1f982a539e8c81dd7e380
Harsh Malviya <harshmalviya72@gmail.com>
2022-02-09 01:11:45 +0530

    Added search functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,6 +37,7 @@ function App() {
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           <Route path="/admin/orderlist" component={OrderListScreen} />
+          <Route path="/search/:keyword" component={Homescreen} />
           <Route path="/" component={Homescreen} exact />
         </Container>
       </main>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,13 +6,14 @@ import { listProducts } from '../actions/productActions';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 
-const Homescreen = () => {
+const Homescreen = ({ match }) => {
+  const keyword = match.params.keyword;
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { error, loading, products } = productList;
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <>
